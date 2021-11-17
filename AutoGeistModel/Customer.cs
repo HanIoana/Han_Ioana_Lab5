7cf33297ebebf14656049f942b68f6f1fadfa781
Ioana Bianca Han <Han.Io.Ioana@student.utcluj.ro>
2021-11-18 00:16:05 +0200

    Add project files.

---
+++ AutoGeistModel/Customer.cs
@@ -0,0 +1,24 @@
+namespace AutoGeistModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Customer")]
+    public partial class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CustId { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public DateTime? PurchaseDate { get; set; }
+    }
+}
